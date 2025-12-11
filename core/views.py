1d3e98b4e52cdac0e68d1bb2c6115834506456e2
AniSantiCop <ana.santibanez07@inacapmail.cl>
2025-12-11 01:12:21 -0300

    views: implementacion ModelViewSets para CRUD

--- core/views.py
+++ core/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+# core/views.py
+from rest_framework import viewsets
+from .models import Empresa, Equipo, Tecnico, PlanMantencion, OrdenTrabajo
+from .serializers import (
+    EmpresaSerializer,
+    EquipoSerializer,
+    TecnicoSerializer,
+    PlanMantencionSerializer,
+    OrdenTrabajoSerializer
+)
+
+class EmpresaViewSet(viewsets.ModelViewSet):
+    queryset = Empresa.objects.all()
+    serializer_class = EmpresaSerializer
+
+
+class EquipoViewSet(viewsets.ModelViewSet):
+    queryset = Equipo.objects.all()
+    serializer_class = EquipoSerializer
+
+
+class TecnicoViewSet(viewsets.ModelViewSet):
+    queryset = Tecnico.objects.all()
+    serializer_class = TecnicoSerializer
+
+
+class PlanMantencionViewSet(viewsets.ModelViewSet):
+    queryset = PlanMantencion.objects.all()
+    serializer_class = PlanMantencionSerializer
+
+
+class OrdenTrabajoViewSet(viewsets.ModelViewSet):
+    queryset = OrdenTrabajo.objects.all()
+    serializer_class = OrdenTrabajoSerializer
